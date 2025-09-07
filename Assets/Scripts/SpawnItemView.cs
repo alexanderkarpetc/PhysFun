20509f172cfeb21eee4cd34b825b211065b7dadf
Oleksandr Karpets <o.karpets@shgames.gg>
2025-09-07 13:41:38 +0200

    Add drag and drop

--- Assets/Scripts/SpawnItemView.cs
+++ Assets/Scripts/SpawnItemView.cs
@@ -3,28 +3,54 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class SpawnItemView : MonoBehaviour, IPointerClickHandler
+public class SpawnItemView : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Action<SpawnItemView> OnClick;
+    public Action<SpawnItemView> OnBeginDragEvent;
+    public Action<SpawnItemView> OnEndDragEvent;
 
     [SerializeField] private Image _image;
     [SerializeField] private GameObject _selectedBg;
 
     public Sprite Sprite => _image.sprite;
 
-    public void Init(Sprite image)
+    private RectTransform _ghost;
+    private Canvas _rootCanvas;
+
+    public void Init(Sprite image) => _image.sprite = image;
+    public void Select(bool value) => _selectedBg.SetActive(value);
+
+    public void OnPointerClick(PointerEventData eventData) => OnClick?.Invoke(this);
+
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        _image.sprite = image;
+        if (_rootCanvas == null) _rootCanvas = GetComponentInParent<Canvas>().rootCanvas;
+
+        var go = new GameObject("DragGhost", typeof(RectTransform), typeof(CanvasGroup), typeof(Image));
+        _ghost = (RectTransform)go.transform;
+        _ghost.SetParent(_rootCanvas.transform, false);
+        _ghost.sizeDelta = ((RectTransform)transform).rect.size;
+        _ghost.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+
+        go.GetComponent<Image>().enabled = false;
+
+        var cg = go.GetComponent<CanvasGroup>();
+        cg.blocksRaycasts = false;
+        cg.alpha = 0.8f;
+
+        _ghost.position = eventData.position;
+
+        OnBeginDragEvent?.Invoke(this);
     }
 
-    public void Select(bool value)
+    public void OnDrag(PointerEventData eventData)
     {
-        _selectedBg.SetActive(value);
+        if (_ghost) _ghost.position = eventData.position;
     }
 
-    public void OnPointerClick(PointerEventData eventData)
+    public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("Image clicked: " + gameObject.name);
-        OnClick?.Invoke(this);
+        if (_ghost) Destroy(_ghost.gameObject);
+        OnEndDragEvent?.Invoke(this);
     }
 }
--- Assets/Scripts/Spawners/ObjectSpawner.cs
+++ Assets/Scripts/Spawners/ObjectSpawner.cs
@@ -1,11 +1,9 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using Spawners;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using Button = UnityEngine.UI.Button;
 
 public class ObjectSpawner : MonoBehaviour
 {
@@ -18,48 +16,101 @@ public class ObjectSpawner : MonoBehaviour
     private List<SpawnItemView> _views = new();
     private SpawnItemView _currentView;
 
+    private GameObject _preview;
+    private float _angleDeg;
+
     private void Start()
     {
         var sprites = Resources.LoadAll<Sprite>("Sprites").ToList();
         _lodText.text = $"Simplification level {_slider.value}";
-        sprites.ForEach(x =>
+
+        foreach (var s in sprites)
         {
-            var itemView = Instantiate(_spawnPrefab, _content);
-            itemView.Init(x);
-            itemView.OnClick += OnItemSelected;
-            _views.Add(itemView);
-        });
+            var v = Instantiate(_spawnPrefab, _content);
+            v.Init(s);
+            v.OnClick += OnItemSelected;
+            v.OnBeginDragEvent += BeginPreviewFromItem;
+            v.OnEndDragEvent += EndPreviewFromItem;
+            _views.Add(v);
+        }
     }
 
     private void OnEnable()
     {
-        _spawnButton.onClick.AddListener(Spawn);
+        _spawnButton.onClick.AddListener(SpawnCurrent);
         _slider.onValueChanged.AddListener(ChangeLod);
     }
 
     private void OnDisable()
     {
-        _spawnButton.onClick.RemoveListener(Spawn);
+        _spawnButton.onClick.RemoveListener(SpawnCurrent);
         _slider.onValueChanged.RemoveListener(ChangeLod);
     }
 
-    private void Spawn()
+    private void Update()
     {
-        if (_currentView == null)
-            return;
-        
-        SpriteFactory.Create(_currentView.Sprite, new Vector3(), null, false, (int)_slider.value);
+        if (_preview == null) return;
+
+        // move preview with mouse
+        var pos = MouseWorld();
+        _preview.transform.position = pos;
+
+        // rotate with wheel
+        float delta = Input.mouseScrollDelta.y;
+        if (Mathf.Abs(delta) > 0.0001f)
+        {
+            _angleDeg += delta * 15f; // step per tick
+            _preview.transform.rotation = Quaternion.Euler(0f, 0f, _angleDeg);
+        }
     }
-    
-    private void OnItemSelected(SpawnItemView selectedItem)
+
+    private void BeginPreviewFromItem(SpawnItemView item)
+    {
+        _currentView = item;
+        _angleDeg = 0f;
+
+        // make a semi-transparent preview
+        _preview = SpriteFactory.Create(item.Sprite, MouseWorld(), null, false, (int)_slider.value);
+        var sr = _preview.GetComponent<SpriteRenderer>();
+        var c = sr.color; c.a = 0.7f; sr.color = c;
+        var rb = _preview.GetComponent<Rigidbody2D>(); if (rb) rb.simulated = false; // no physics during preview
+    }
+
+    private void EndPreviewFromItem(SpawnItemView item)
+    {
+        if (_preview == null) return;
+
+        // finalize: make fully opaque + enable physics
+        var sr = _preview.GetComponent<SpriteRenderer>();
+        var c = sr.color; c.a = 1f; sr.color = c;
+
+        var rb = _preview.GetComponent<Rigidbody2D>(); if (rb) rb.simulated = true;
+
+        _preview = null; // keep spawned object in scene
+    }
+
+    private void SpawnCurrent()
+    {
+        if (_currentView == null) return;
+        SpriteFactory.Create(_currentView.Sprite, Vector3.zero, null, false, (int)_slider.value);
+    }
+
+    private void OnItemSelected(SpawnItemView selected)
     {
         _currentView?.Select(false);
-        selectedItem.Select(true);
-        _currentView = selectedItem;
+        selected.Select(true);
+        _currentView = selected;
     }
-    
-    private void ChangeLod(float value)
+
+    private void ChangeLod(float value) => _lodText.text = $"Simplification level {_slider.value}";
+
+    private static Vector3 MouseWorld()
     {
-        _lodText.text = $"Simplification level {_slider.value}";
+        var cam = Camera.main;
+        var sp = Input.mousePosition;
+        sp.z = -cam.transform.position.z; // distance to Z=0 plane
+        var wp = cam.ScreenToWorldPoint(sp);
+        wp.z = 0f;
+        return wp;
     }
 }
